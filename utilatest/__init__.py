f122b185a0154e10d863829a793c1ad8f5687e68
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-21 17:07:16 +0200

    0.1.2 automatically generated release

--- utilatest/__init__.py
+++ utilatest/__init__.py
@@ -39,7 +39,7 @@ from utilatest.select import skip_virtual
 # utils
 from utilatest.utils import open_webbrowser
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PACKAGE = 'utilatest'
