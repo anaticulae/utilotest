c78878d01c96f14b426e7b1284d41060e8cc1faf
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-15 19:50:25 +0100

    test(view): add test to verify provided fixture

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,12 +1,20 @@
 # =============================================================================
 # C O P Y R I G H T
 # -----------------------------------------------------------------------------
-# Copyright (c) 2020-2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
 # This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-from viewvisitor.fixture import selenium_driver  # pylint:disable=W0611
+import utilatest
 
-pytest_plugins = ['pytester', 'xdist', 'localserver']  # pylint: disable=invalid-name
+
+@utilatest.longrun
+@utilatest.displayed
+def test_selenium_driver(selenium_driver, httpserver):
+    httpserver.serve_content('<div id="helmut">Helmut is talking</div>')
+    # visit faked page
+    selenium_driver.get(httpserver.url)
+    text = selenium_driver.find_element_by_id('helmut').text
+    assert text == 'Helmut is talking'
