43008fcf7985d8306506a7bce76c9f5731b1ac18
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-10-12 20:03:08 +0200

    test(enable): enable after improving

--- tests/test_baseline.py
+++ tests/test_baseline.py
@@ -7,8 +7,6 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import sys
-
 import pytest
 import utila
 
@@ -30,10 +28,6 @@ class Simple(utilatest.BaseLiner):
         )
 
 
-@pytest.mark.skipif(
-    not sys.platform.startswith("win"),
-    reason='pdfcat does not work on linux',
-)
 def test_baseline_mixin_valid(td):
     td.mkdir('archive')
     utila.run('power -h >> archive/message')
