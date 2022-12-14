5381fc9c9b0786b98776f0937ae12c6ba238fefc
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-14 15:03:31 +0000

    chore(setup): use quick installer

--- setup.py
+++ setup.py
@@ -8,49 +8,15 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-UTF8 = 'utf8'
-
-with open(os.path.join(ROOT, 'README.md'), encoding=UTF8) as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'utilatest/__init__.py'), encoding=UTF8) as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding=UTF8) as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
-
+PACKAGES = [
+    'utilatest',
+    'utilatest.cli',
+    'viewvisitor',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'collect_trace=utilatest.cli.tracer:main',
+])
 if __name__ == "__main__":
-    # allow ssetup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='some useful test operation',
-        install_requires=REQUIRES,
-        include_package_data=True,
-        long_description=README,
-        name='utilatest',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/utilatest',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-        ],
-        packages=[
-            'utilatest',
-            'utilatest.cli',
-            'viewvisitor',
-        ],
-        entry_points={
-            'console_scripts': ['collect_trace = utilatest.cli.tracer:main',],
-        },
-    )
+    utila.install(__file__)
