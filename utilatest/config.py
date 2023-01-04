587589f15c735c56e1d3891b59d2d72bb6cc08af
Helmut Konrad Schewe <helmutus@outlook.com>
2023-01-04 12:43:18 +0000

    docs(copyright): Happy New Year!

--- utilatest/config.py
+++ utilatest/config.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # C O P Y R I G H T
 # -----------------------------------------------------------------------------
-# Copyright (c) 2022 by Helmut Konrad Schewe. All rights reserved.
+# Copyright (c) 2022-2023 by Helmut Konrad Schewe. All rights reserved.
 # This file is property of Helmut Konrad Schewe. Any unauthorized copy,
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
